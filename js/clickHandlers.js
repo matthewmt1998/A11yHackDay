189830109692a9b5863e031d9fd1d2b3d93e87f6
Rick Threlfall <rickthrelfall@Ricks-MacBook-Air.local>
2018-02-15 13:34:21 +0000

    Click handlers basic

---
+++ js/clickHandlers.js
@@ -0,0 +1,25 @@
+document.getElementById("a11y_id_btn_effect_none").addEventListener("click", effectNone);
+document.getElementById("a11y_id_btn_effect_surface").addEventListener("click", effectSurface);
+document.getElementById("a11y_id_btn_effect_phonological").addEventListener("click", effectPhonological);
+document.getElementById("a11y_id_btn_effect_visual").addEventListener("click", effectVisual);
+document.getElementById("a11y_id_btn_effect_dyscalculia").addEventListener("click", effectDyscalculia);
+
+function effectNone() {
+    alert ("Hello World!");
+}
+
+function effectSurface() {
+    alert ("Hello World!");
+}
+
+function effectPhonological() {
+    alert ("Hello World!");
+}
+
+function effectVisual() {
+    alert ("Hello World!");
+}
+
+function effectDyscalculia() {
+    alert ("Hello World!");
+}
