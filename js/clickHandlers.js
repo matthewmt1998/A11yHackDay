91a0d8a5684cc0b5b4e17aae9df727aef8c3686c
Rick Threlfall <rickthrelfall@Ricks-MacBook-Air.local>
2018-02-15 16:05:27 +0000

    git really is fucking shite

--- js/clickHandlers.js
+++ js/clickHandlers.js
@@ -54,24 +54,3 @@ function addEffect(id) {
     }
 }
 
-function effectNone() {
-}
-
-function effectSurface() {
-}
-
-function effectPhonological() {
-    var x = document.createElement('script');
-    x.src = 'js/wordScrambler.js';
-    document.getElementsByTagName("head")[0].removeChild(x);
-}
-
-function effectVisual() {
-    var x = document.createElement('script');
-    x.src = 'js/wordScrambler.js';
-    document.getElementsByTagName("head")[0].appendChild(x);
-}
-
-function effectDyscalculia() {
-}
-
